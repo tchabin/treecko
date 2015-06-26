8ab970aebe3c620ab0ced5eae6aeeb0b2abdd598
Thomas Chabin <T.chabin@hotmail.fr>
2015-06-26 17:48:09 +0200

    fork new fitness new contraint

---
+++ fitness.py
@@ -0,0 +1,69 @@
+# Simple script to run fitness evaluation for the individuals in 2048
+# but with some command-line parameters
+
+import argparse
+import os
+import subprocess
+import sys
+
+#DEBUG = True
+DEBUG = False
+
+# parse some command-line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--input", required=True)
+parser.add_argument("--output", required=True)
+
+args = parser.parse_args()
+
+if DEBUG :
+	print "Input file: \"" + args.input + "\""
+	print "Output file: \"" + args.output + "\""
+
+individualName = args.input
+fitnessFileName = args.output
+
+# create a temporay directory, copy all the relevant files inside it and move there
+individualDir = individualName + "-dir"
+os.system("mkdir " + individualDir)
+os.system("cp replace.py 2048.jar " + individualName + " " + individualDir)
+os.chdir(individualName + "-dir")
+
+# compile and execute: every line is basically what you would write on the terminal, only inside the os.system() system call
+os.system("./replace.py " + individualName + " MyAgent.java")
+os.system("javac -cp 2048.jar MyAgent.java")
+os.system("jar -cf MyAgent.jar MyAgent.class")
+
+# subprocess is used to capture the stdout inside a string
+stdOutput = subprocess.Popen("java -jar 2048.jar MyAgent.jar MyAgent 100 1.0 123", stdout = subprocess.PIPE, #stderr = subprocess.STDOUT, 
+shell=True).communicate()[0]
+
+if DEBUG :
+	print "The captured standard output is: \"" + stdOutput + "\""
+
+# parse the two lines
+lines = stdOutput.rstrip().split("\n")
+header = lines[0].rstrip().split(',')
+values = lines[1].rstrip().split(',')
+
+if DEBUG :
+	print "I just parsed these values: header=", header
+	print "I just parsed these values: header=", values
+
+# create a hash map with the headers
+fitnessValues = dict()
+for i in range(0, len(header)) :
+	fitnessValues[header[i]] = float(values[i])
+
+if DEBUG : print "The dictionary is:", fitnessValues
+
+# write the fitness file
+with open(fitnessFileName, "w") as fp :
+	fp.write( str(fitnessValues["MeanScore"]) + " " + str(fitnessValues["AvgActionTime[ms]"]) )
+
+# copy the fitness file to the parent directory
+os.system("cp " + fitnessFileName + " ..")
+
+# remove all temporary files
+os.chdir("..")
+os.system("rm -r " + individualDir)
---
+++ fork.py
@@ -0,0 +1,78 @@
+# Python script to execute multiple processes and collect their results in a single file
+# by Alberto Tonda, 2014 <alberto.tonda@grignon.inra.fr>
+
+import argparse
+import os
+
+# NOTE "global" variables; modify here to change the behavior of the script
+scriptCommand = "python fitness.py" 	# name of the script, e.g. "python script.py"
+inputOption = "--input "	# options of the script
+outputOption = "--output "
+otherOptions = "" 
+
+childOutputFileBase = "fitness"		# the base name for the children's output files (they will become fitness1, fitness2, etc.)
+
+defaultOutputFile = "fitness.out" # the default name for the output file produced by this script
+
+# ok, as input we need
+# - name of the output file
+# - list of arguments to pass to each child
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--output", type=str, help="Name of the output file, where the results of all children will be written to. By default, it's " + defaultOutputFile) 
+
+parser.add_argument("--input", metavar='N', type=str, nargs='+', required=True, help="Input filenames. Each one will be passed to a child.")
+
+args = parser.parse_args()
+
+# check if a different output file was specified
+if args.output != None : defaultOutputFile = args.output
+
+# some useful variables
+outputFilesList = []
+pidList = []
+
+# now, for each argument passed as "input", create a child and execute a command
+for i in range(0, len(args.input)) :
+	
+	# input and output for the child
+	childInputFile = args.input[i]
+	childOutputFile = childOutputFileBase + str(i)
+	
+	# fork the process
+	newpid = os.fork()
+
+	if newpid == 0 :
+		# child
+		# create command
+		command = scriptCommand + " " + inputOption + childInputFile + " " + outputOption + childOutputFile
+		# execute it
+		#with open(childOutputFile, "w") as fp : fp.write( command + "\n" )
+		os.system(command)
+		os._exit(0)
+	else :
+		# father
+		# store the PID and the filename
+		outputFilesList.append( childOutputFile )
+		pidList.append( newpid )
+
+# wait for all child processes to exit
+for pid in pidList :
+	os.waitpid( pid, 0 ) # TODO check for possible errors returned by the child process
+
+# write final output file
+with open(defaultOutputFile, "w") as fp :
+	
+	for outputFile in outputFilesList :
+		# read the file
+		lines = []
+		with open(outputFile, "r") as fpChild : lines = fpChild.readlines()
+		
+		# copy the file content to the new file
+		fp.write(lines[0].rstrip() + "\n")
+		
+		# delete the temporary file
+		os.remove( outputFile )
+
+# end
